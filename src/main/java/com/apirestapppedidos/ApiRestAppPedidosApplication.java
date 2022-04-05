0f575bda701db04532320a7d0325e7107fd207a3
Silas Santos <sillassouza.ads@gmail.com>
2022-04-05 02:38:16 -0300

    Criação do projeto

---
+++ src/main/java/com/apirestapppedidos/ApiRestAppPedidosApplication.java
@@ -0,0 +1,13 @@
+package com.apirestapppedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestAppPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestAppPedidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apirestapppedidos/ApiRestAppPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apirestapppedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestAppPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
